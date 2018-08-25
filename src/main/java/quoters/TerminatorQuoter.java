32b0e47baccb7693c85f17bff76d6110ed1a0fcd
Denis Kozlov <ConstrayRUS@gmail.com>
2018-08-25 19:39:59 +0400

    Post Construct

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,23 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
     private String message;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     public void sayQuote() {
         for (int i = 0; i < repeat; i++) {
