ad1e9219811ef31a50586b8f41a92152489b3d04
Denis Kozlov <ConstrayRUS@gmail.com>
2018-08-25 21:16:44 +0400

    Somehow this didn't work with beanfactoryaware implementation

--- src/main/java/quoters/PostProxyInvokerContextListener.java
+++ src/main/java/quoters/PostProxyInvokerContextListener.java
@@ -1,31 +1,35 @@
 package quoters;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
-public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent> {
+@Component
+public class PostProxyInvokerContextListener implements ApplicationListener<ContextRefreshedEvent>, BeanFactoryAware {
 
-    @Autowired
     private ConfigurableListableBeanFactory beanFactory;
 
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         ApplicationContext context = contextRefreshedEvent.getApplicationContext();
-        String[] beanDefinitionNames = context.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            final BeanDefinition beanDefinition = beanFactory.getBeanDefinition(beanDefinitionName);
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names) {
+            final BeanDefinition beanDefinition = beanFactory.getBeanDefinition(name);
             final String beanClassName = beanDefinition.getBeanClassName();
             try {
                 final Class<?> beanClass = Class.forName(beanClassName);
                 final Method[] methods = beanClass.getMethods();
                 for (Method method : methods) {
                     if(method.isAnnotationPresent(PostProxy.class)) {
-                        final Object bean = context.getBean(beanDefinitionName);
+                        final Object bean = context.getBean(name);
                         final Method currentMethod = bean.getClass().getMethod(method.getName(), method.getParameterTypes());
                         currentMethod.invoke(bean);
                     }
@@ -35,5 +39,10 @@ public class PostProxyInvokerContextListener implements ApplicationListener<Cont
             }
 
         }
+    }
+
+    @Autowired
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = (ConfigurableListableBeanFactory) beanFactory;
     }
 }
